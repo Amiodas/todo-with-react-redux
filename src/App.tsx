8eb7a93ea32d52e1d6ad408481fe5222a0eac87c
Amiodas <amio.das.dip@gmail.com>
2024-08-20 03:59:48 +0600

    #initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <h1>Todo</h1>
+      <Button>Add todo</Button>
+    </>
+  )
+}
+
+export default App
